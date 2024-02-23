9756b62d81f7868ed02290a20c397b447fd28ccf
baldTurkey <miglanishorya@gmail.com>
2024-02-23 15:24:56 -0500

    Gif now has clickable events

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-// Get the video element
-const gifVideo = document.getElementById('gifVideo');
 
-// Add event listener for click
-gifVideo.addEventListener('click', function() {
-    // Check if video is paused
-    if (gifVideo.paused) {
-        // Play the video
-        gifVideo.play();
-    } else {
-        // Pause the video
-        gifVideo.pause();
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("JavaScript file linked properly."); // Add this line
+    
+    // Get a reference to the GIF image
+    var gifImage = document.getElementById('gifImage');
+    
+    // Flag to track if the GIF is currently playing
+    gifImage.src = 'assets/moon_gif.gif';
+    var isPlaying = true;
+    
+    // Add click event listener to the GIF image
+    gifImage.addEventListener('click', function() {
+        gifImage.src = 'assets/moon_gif_real.gif';
+        isPlaying = false;
+
+        if(!isPlaying) {
+            setTimeout(function() {
+                gifImage.src = 'assets/moon_gif_end.png';
+
+                gifImage.removeEventListener('click', handleClick);
+
+            }, 4830);
+        }
+
+
+        
+        
+        // Toggle the playing flag
+        isPlaying = !isPlaying;
+    });
 });
 
-// Add event listener for ended
-gifVideo.addEventListener('ended', function() {
-    // Pause the video
-    gifVideo.pause();
-});
+
+
+
